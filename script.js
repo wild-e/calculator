ed2532fd63c2cd064432ddace702c71f14f69b93
JeremyMordo <jeje.mordo@orange.fr>
2020-09-26 22:47:37 +0200

    modification fichier

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
-let one = document.getElementById('one');
-let two = document.getElementById('two');
+/**Definition des variables "chiffre" */
+
+let zero = document.getElementById('zero').value;
+let one = document.getElementById('one').value;
+let two = document.getElementById('two').value;
+let three = document.getElementById('three').value;
+let four = document.getElementById('four').value;
+let five = document.getElementById('five').value;
+let six = document.getElementById('six').value;
+let seven = document.getElementById('seven').value;
+let eight = document.getElementById('eight').value;
+let nine = document.getElementById('nine').value;
+
+let number=[zero, one, two, three, four, five, six, seven, eight, nine];
+
+/**Definition des variables "operateur" */
+
 let sum = document.getElementById('sum');
 let equal = document.getElementById('equal');
+let substract = document.getElementById('substract');
+let divide = document.getElementById('divide');
+let multiply = document.getElementById('multiply');
+
+let operator=[sum, equal, substract, divide, multiply];
+
+/**Definition de l'endroit où seront écrites les valeurs */
+
 let output = document.getElementById('output');
-let outputArrayOne = "";
-let operator = "";
-let outputArrayTwo = "";
 
-let grid = [];
-console.log(grid.push(one));
-
-
-function calculator(a){
-    outputArrayOne = outputArrayOne + a.value;
-    output.innerHTML = outputArrayOne;
-};
-
-for (let i = 0; i <= grid.length; i++){
-        grid[i].onclick = function(){
-            calculator(grid[i]);
+for (let i=0;i<number.length;i++){
+    number[i].onclick = function(){        /**Ecriture du/des premier(s) chiffre(s)*/
+        output.innerHTML += number[i].value;
+        let premier=output;
+        for (let j=0;j<operator.length;j++){       /**Ecriture des operateurs*/
+            operator[j].onclick = function(){
+                output.innerHTML += operator[j].value;
+                for (let h=0;h<number.length;h++){        /**Ecriture du/des deuxieme(s) chiffre(s)*/
+                    number[h].onclick = function(){
+                        output.innerHTML += number[h].value;
+                        let deuxieme=output;
+                        equal.onclick = function(){
+                            output.innerHTML= premier + deuxieme;
+                        };
+                    };
+                }
+            };
         }
-};
-
-
-   
-
-
-sum.onclick = function(){
-    operator = sum.value;
-    if (outputArrayOne != ""){
-        output.innerHTML = sum.value;
-    }
-    one.onclick = function(){
-        outputArrayTwo = outputArrayTwo + one.value;
-        output.innerHTML = outputArrayTwo;
     };
-};
+}
 
 
-equal.onclick = function(){
-    if (operator == "+") {
-    output.innerHTML = parseInt(outputArrayOne) + parseInt(outputArrayTwo);
-    }
-};
 
 
